dd5b3111bf68d5e49b151730afe93c8e4693b207
Jenea Vranceanu <jeneavranceanu@gmail.com>
2020-06-16 18:32:33 +0300

    Adapter updated to produce two types of view holders

--- app/src/main/java/pl/nynacode/naukapraw/MyAdapter.kt
+++ app/src/main/java/pl/nynacode/naukapraw/MyAdapter.kt
@@ -7,54 +7,79 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.cart_view_legal_name.view.*
 import kotlinx.android.synthetic.main.chapter_layout.view.*
 
-private const val TITLE_VIEW =0
-private const val ARTICLE_VIEW=1
+/**
+ * Contains all possible view holder types of [MyAdapter].
+ */
+sealed class Types(val rawType: Int) {
+    object Title : Types(0)
+    object Article : Types(1)
+
+    companion object {
+        fun from(rawType: Int) =
+            when (rawType) {
+                Title.rawType -> Title
+                Article.rawType -> Article
+                else -> throw RuntimeException("No such type")
+            }
+    }
+}
 
 class MyAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if (viewType== TITLE_VIEW){
-            val view = LayoutInflater.from(parent.context)
-            val cardArticle = view.inflate(R.layout.chapter_layout, parent, false)
-            return TitleViewHolder(cardArticle)
-        }else{
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.cart_view_legal_name, parent, false)
-            return ArticleViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        LayoutInflater.from(parent.context).let { inflater ->
+            when (Types.from(viewType)) {
+                Types.Title ->
+                    TitleViewHolder(
+                        inflater.inflate(
+                            R.layout.chapter_layout,
+                            parent,
+                            false
+                        )
+                    )
+                Types.Article ->
+                    ArticleViewHolder(
+                        inflater.inflate(
+                            R.layout.cart_view_legal_name,
+                            parent,
+                            false
+                        )
+                    )
+            }
         }
-    }
 
     override fun getItemCount(): Int {
         return KodeksKarny.nrArticle.size
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return if (KodeksKarny.nrArticle[position] == 0)
-        {
-            TITLE_VIEW
-        }else {
-            ARTICLE_VIEW}
-    }
+    override fun getItemViewType(position: Int): Int =
+        when (position) {
+            0 -> Types.Title.rawType
+            else -> Types.Article.rawType
+        }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (getItemViewType(position) == TITLE_VIEW){
-            val chapterName = holder.itemView.tvChapterName
-            chapterName.setText(KodeksKarny.nrArticle[position])
-        }else {
-            val nrArt = holder.itemView.nrArt
-            nrArt.setText(KodeksKarny.nrArticle[position])
-            val txtArt = holder.itemView.txtArt
-            txtArt.setText(KodeksKarny.txtArticle[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) =
+        when (Types.from(getItemViewType(position))) {
+            Types.Title -> {
+                val chapterName = holder.itemView.tvChapterName
+                chapterName.text = KodeksKarny.nrArticle[position]
+            }
+            Types.Article -> {
+                val nrArt = holder.itemView.nrArt
+                nrArt.text = KodeksKarny.nrArticle[position]
+                val txtArt = holder.itemView.txtArt
+                txtArt.text = KodeksKarny.txtArticle[position]
 
-            nrArt.setOnClickListener{
-                if (txtArt.visibility == View.GONE){
-                    txtArt.visibility = View.VISIBLE
-                }else{
-                    txtArt.visibility = View.GONE
+                nrArt.setOnClickListener {
+                    if (txtArt.visibility == View.GONE) {
+                        txtArt.visibility = View.VISIBLE
+                    } else {
+                        txtArt.visibility = View.GONE
+                    }
                 }
             }
         }
-    }
 }
 
-class TitleViewHolder(val view: View):RecyclerView.ViewHolder(view){
-}
-class ArticleViewHolder(val view: View):RecyclerView.ViewHolder(view){}
+class TitleViewHolder(view: View) : RecyclerView.ViewHolder(view)
+class ArticleViewHolder(view: View) : RecyclerView.ViewHolder(view)
